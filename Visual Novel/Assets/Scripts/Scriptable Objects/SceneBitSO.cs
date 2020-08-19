677a02798187763607ede3d94c9e2a5e9edffcbd
Lucio Ruixo <lucioruixo@gmail.com>
2020-08-18 22:41:31 -0300

    Decisions and scene bit progression added

---
+++ Visual Novel/Assets/Scripts/Scriptable Objects/SceneBitSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Scene Bit", menuName = "Scene Bit")]
+public class SceneBitSO : ScriptableObject
+{
+    public SceneBitManager.SceneBitTypes type;
+
+    public SceneBitManager.DialogueData dialogueData;
+    public SceneBitManager.DecisionData decisionData;
+}
