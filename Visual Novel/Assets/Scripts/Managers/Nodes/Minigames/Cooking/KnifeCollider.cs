fb23f4ef70c4c7775d421ae37b0dd0554804a75f
Lucio Ruixo <lucioruixo@gmail.com>
2020-09-16 21:25:38 -0300

    Complete #591

--- Visual Novel/Assets/Scripts/Managers/Nodes/Minigames/Cooking/KnifeCollider.cs
+++ Visual Novel/Assets/Scripts/Managers/Nodes/Minigames/Cooking/KnifeCollider.cs
@@ -2,8 +2,20 @@
 
 public class KnifeCollider : MonoBehaviour
 {
+    bool collisioning = false;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(":)");
+        if (collision.tag == "Food") collisioning = true;
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Food") collisioning = false;
+    }
+
+    void Update()
+    {
+        if (collisioning && Input.GetButtonDown("Left Click")) Debug.Log("Collision");
     }
 }
